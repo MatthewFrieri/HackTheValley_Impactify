950da880459d6f0ff29e74ef2d01d27238190568
David Crimi <crimid01@gmail.com>
2024-10-05 12:46:15 -0400

    auth view

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-    path('login/', views.LoginView.as_view(), name='login'),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
     path('register/', views.RegisterView.as_view(), name='register'),
     path('session/data/', views.SessionDataView.as_view(), name='session_data'),
     path('session/start/', views.SessionStartView.as_view(), name='session_start'),
     path('session/end/', views.SessionStopView.as_view(), name='session_end'),
+
 ]
